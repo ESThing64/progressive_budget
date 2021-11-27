75d365bfcf90ee4832c2f55c419cb919956314c7
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-27 11:13:41 -0600

    changed indexeddb structure

---
+++ public/service-worker.js
@@ -0,0 +1,63 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/topic.html',
+    '/assets/css/style.css',
+    '/index.js',
+    'public/icons/icon-192x192.png',
+    'public/icons/icon-512x512.png'
+
+  ];
+  
+  const PRECACHE = 'precache-v1';
+  const DATA = 'runtime';
+  
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  // CLEARS OLD CACHE
+  self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, DATA];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(DATA).then((cache) => {
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
+  
